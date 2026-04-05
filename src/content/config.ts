b835d644cef3023c2e21a85414f59c6e38b742ee
Jhon Montenegro <jhonamacarol@gmail.com>
2026-04-04 23:54:52 -0400

    homepage editável pelo CMS

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,4 +13,33 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const paginas = defineCollection({
+  type: 'content',
+  schema: z.object({
+    heroTag: z.string(),
+    heroTitulo: z.string(),
+    heroDescricao: z.string(),
+    heroBotaoPrimario: z.string(),
+    heroBotaoSecundario: z.string(),
+    card1Fase: z.string(),
+    card1Texto: z.string(),
+    card2Fase: z.string(),
+    card2Texto: z.string(),
+    card3Fase: z.string(),
+    card3Texto: z.string(),
+    secaoCicloTitulo: z.string(),
+    secaoCicloDescricao: z.string(),
+    manifestoTitulo: z.string(),
+    manifestoDescricao: z.string(),
+    valor1Titulo: z.string(),
+    valor1Texto: z.string(),
+    valor2Titulo: z.string(),
+    valor2Texto: z.string(),
+    valor3Titulo: z.string(),
+    valor3Texto: z.string(),
+    newsletterTitulo: z.string(),
+    newsletterDescricao: z.string(),
+  }),
+});
+
+export const collections = { blog, paginas };
